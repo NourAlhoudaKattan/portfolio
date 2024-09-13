1032dc2f7fa10c667fded200dfbbd9c71b504edc
NourAlhoudaKattan <nwralhdyqtan14@gmail.com>
2024-09-13 13:12:48 +0300

    7

--- src/Component/Servic/Servic.jsx
+++ src/Component/Servic/Servic.jsx
@@ -6,30 +6,27 @@ import ux from "../../assets/img/ux.png";
 import web from "../../assets/img/web.png";
 import "./Servic.css";
 const Servic = () => {
+  
+    let servic = [
+      {id:1,img:ui,text:"I create user-friendly designs to make your app or website look great and easy to use and frindlly"},
+      {id:2,img:ux,text:"I specialize in designing attractive user interfaces to make your app or website stand out with a unique" },
+      {id:3,img:web,text:"I create user-friendly designs for websites as a Frontend Developer to make sure users have a smooth"  }
+    ];
   const [{theme,isDark},toggleTheme]=useContext(ThemeContext)
   console.log(theme)
   return (
     <div className="pserv" id="Service"  style={{ backgroundColor: theme.backgroundImage,color: theme.color }}>
       <h1  className={isDark?'titledark':'titlelight'}>Service</h1>
       <div className="serv">
-        <div className="card-serv" data-aos="flip-left"
-     data-aos-easing="ease-out-cubic"
-     data-aos-duration="2000">
-          <img src={ui} alt="" />
-          <p> I create user-friendly designs to make your app or website look great and easy to use and frindlly</p>
+      {servic.map(e=>{
+        return   <div className="card-serv" data-aos="flip-left"  data-aos-easing="ease-out-cubic" data-aos-duration="2000">
+                  
+           <img src={e.img} alt="" />
+           <p> {e.text}</p>
         </div>
-        <div className="card-serv" data-aos="flip-left"
-     data-aos-easing="ease-out-cubic"
-     data-aos-duration="2000">
-          <img src={ux} alt="" />
-          <p>I specialize in designing attractive user interfaces to make your app or website stand out with a unique </p>
-        </div>
-        <div className="card-serv" data-aos="flip-left"
-     data-aos-easing="ease-out-cubic"
-     data-aos-duration="2000">
-          <img src={web} alt="" />
-          <p>I create user-friendly designs for websites as a Frontend Developer to make sure users have a smooth </p>
-        </div>
+      })
+
+      }
       </div>
     </div>
   );
